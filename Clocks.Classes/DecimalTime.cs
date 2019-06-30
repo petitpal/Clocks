7ff131b23bc75025e3cba56c081143df527d745a
Paul Newman <paul.newman@innovise.com>
2019-06-30 12:10:55 +0100

    Adding and fixing Ms + more tests

--- Clocks.Classes/DecimalTime.cs
+++ Clocks.Classes/DecimalTime.cs
@@ -6,46 +6,77 @@ namespace Clocks.Classes
     {
         public const int HoursPerDay = 10;
         public const int MinutesPerHour = 100;
-        public const int SecondsPerHour = 10000;
         public const int SecondsPerMinute = 100;
-        public const double ConvertUtcSecondsToDecimalSeconds = 0.864;
+        public const int MillisecondsPerSecond = 1000;
+
+        // private const int DecMsPerDay = 100000000;
+        // private const int UtcMsPerDay = 86400000;
+
+        private const int MsPerHour = 10000000;
+        private const int MsPerMinute = 100000;
+        private const int MsPerSecond = 1000;
 
         public int Hours { get; set; }
         public int Minutes { get; set; }
         public int Seconds { get; set; }
+        public int Milliseconds { get; set; }
 
-        public DecimalTime Empty()
+        public DecimalTime(int hours, int minutes, int seconds, int milliseconds)
         {
-            return default;
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+            Milliseconds = milliseconds;
         }
 
-        public bool Equals(DecimalTime dc1, DecimalTime dc2)
+        public DecimalTime Empty() =>
+            default;
+
+        public bool Equals(DecimalTime dc1, DecimalTime dc2) =>
+            dc1.Hours == dc2.Hours
+            && dc1.Minutes == dc2.Minutes
+            && dc1.Seconds == dc2.Seconds
+            && dc1.Milliseconds == dc2.Milliseconds;
+
+        public DecimalTime Now() =>
+            FromUtc(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, DateTime.Now.Millisecond);
+
+        public static DecimalTime FromUtc(int hours, int minutes, int seconds, int milliseconds)
         {
-            return (dc1.Hours == dc2.Hours && dc1.Minutes == dc2.Minutes && dc1.Seconds == dc2.Seconds);
+            const double UtcMsToDecimalMs = 0.864;
+
+            var utcTotalMs = new TimeSpan(0, hours, minutes, seconds, milliseconds).TotalMilliseconds;
+            var decTotalMs = utcTotalMs / UtcMsToDecimalMs;
+
+            return new DecimalTime()
+            {
+                Hours = ExtractUnitByMs(ref decTotalMs, MsPerHour),
+                Minutes = ExtractUnitByMs(ref decTotalMs, MsPerMinute),
+                Seconds = ExtractUnitByMs(ref decTotalMs, MsPerSecond),
+                Milliseconds = (int)decTotalMs
+            };
         }
 
-        public DecimalTime Now()
+        private static int ExtractUnitByMs(ref double totalMs, int msPerUnit)
         {
-            return FromUtc(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            var t = (int)(totalMs / msPerUnit);
+            totalMs -= (t * msPerUnit);
+            return t;
         }
 
-        public static DecimalTime FromUtc(int hours, int minutes, int seconds)
-        {
-            var totalUtcSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
-            var totalDecimalSeconds = totalUtcSeconds / ConvertUtcSecondsToDecimalSeconds;
 
-            var decimalHours = (int)totalDecimalSeconds / SecondsPerHour;
-            totalDecimalSeconds -= (decimalHours * SecondsPerHour);
+        //var utcHour = (int)(decTotalMs / MsPerHour);
+        //decTotalMs -= (utcHour * MsPerHour);
 
-            var decimalMinutes = (int)totalDecimalSeconds / SecondsPerMinute;
-            totalDecimalSeconds -= (decimalMinutes * SecondsPerMinute);
+        //var utcMinute = (int)(decTotalMs / MsPerMinute);
+        //decTotalMs -= (utcMinute * MsPerMinute);
 
-            var decimalSeconds = (int)totalDecimalSeconds;
+        //var utcSecond = (int)(decTotalMs / MsPerSecond);
+        //decTotalMs -= (utcSecond * MsPerSecond);
 
-            return new DecimalTime() { Hours = decimalHours, Minutes = decimalMinutes, Seconds = decimalSeconds };
-        }
+        //var utcMs = (int)decTotalMs;
 
-        //public static TimeSpan ToUtc(DecimalTime dc)
+        //public static TimeSpan ToUtc(utcTime dc)
         //{
 
         //}
--- Clocks.Test/DecimalClockTests.cs
+++ Clocks.Test/DecimalClockTests.cs
@@ -6,40 +6,75 @@ namespace ClocksTest
     [TestClass]
     public class DecimalClockTests
     {
-        [TestMethod]
-        public void Midnight()
+        [DataTestMethod]
+        [DataRow(0, 0, 0, 0, 0, 0, 0, 0)]
+        [DataRow(6, 0, 0, 0, 2, 50, 0, 0)]
+        [DataRow(12, 0, 0, 0, 5, 0, 0, 0)]
+        [DataRow(18, 0, 0, 0, 7, 50, 0, 0)]
+        [DataRow(0, 1, 0, 0, 0, 0, 69, 444)]
+        [DataRow(23, 58, 0, 0, 9, 98, 61, 111)]
+        [DataRow(23, 59, 0, 0, 9, 99, 30, 555)]
+        public void UtcToDecimal(int utcHours, int utcMinutes, int utcSeconds, int utcMs,
+                                 int decHours, int decMinutes, int decSeconds, int decMs)
         {
-            DecimalTime testDate = DecimalTime.FromUtc(0, 0, 0);
-            Assert.AreEqual(testDate.Hours, 0);
-            Assert.AreEqual(testDate.Minutes, 0);
-            Assert.AreEqual(testDate.Seconds, 0);
+            // arrange
+
+            // act
+            var dt = DecimalTime.FromUtc(utcHours, utcMinutes, utcSeconds, utcMs);
+            
+            // assert
+            Assert.AreEqual(dt.Hours, decHours);
+            Assert.AreEqual(dt.Minutes, decMinutes);
+            Assert.AreEqual(dt.Seconds, decSeconds);
+            Assert.AreEqual(dt.Milliseconds, decMs);
+        }
+
+
+
+
+        [TestMethod]
+        public void CreateNew()
+        {
+            // arrange
+            var dt = new DecimalTime(1, 2, 3, 4);
+
+            // act
+
+            // assert
+            Assert.AreEqual(dt.Hours, 1);
+            Assert.AreEqual(dt.Minutes, 2);
+            Assert.AreEqual(dt.Seconds, 3);
+            Assert.AreEqual(dt.Milliseconds, 4);
+        }
+
+
+        [TestMethod]
+        public void Equal()
+        {
+            // arrange
+            var dt1 = new DecimalTime(1, 2, 3, 4);
+            var dt2 = new DecimalTime(1, 2, 3, 4);
+
+            // act
+            var areEqual = dt1.Equals(dt2);
+
+            // assert
+            Assert.IsTrue(areEqual);
         }
 
         [TestMethod]
-        public void Six()
+        public void NotEqual()
         {
-            DecimalTime testDate = DecimalTime.FromUtc(6, 0, 0);
-            Assert.AreEqual(testDate.Hours, 2);
-            Assert.AreEqual(testDate.Minutes, 50);
-            Assert.AreEqual(testDate.Seconds, 0);
+            // arrange
+            var dt1 = new DecimalTime(1, 2, 3, 4);
+            var dt2 = new DecimalTime(2, 3, 4, 5);
+
+            // act
+            var areEqual = dt1.Equals(dt2);
+
+            // assert
+            Assert.IsFalse(areEqual);
         }
 
-        [TestMethod]
-        public void Twleve()
-        {
-            DecimalTime testDate = DecimalTime.FromUtc(12, 0, 0);
-            Assert.AreEqual(testDate.Hours, 5);
-            Assert.AreEqual(testDate.Minutes, 0);
-            Assert.AreEqual(testDate.Seconds, 0);
-        }
-
-        [TestMethod]
-        public void Eighteen()
-        {
-            DecimalTime testDate = DecimalTime.FromUtc(18, 0, 0);
-            Assert.AreEqual(testDate.Hours, 7);
-            Assert.AreEqual(testDate.Minutes, 50);
-            Assert.AreEqual(testDate.Seconds, 0);
-        }
     }
 }
